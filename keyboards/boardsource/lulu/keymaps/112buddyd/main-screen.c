b846919738e298f76b3980e60694431ea8ff7a74
Buddy D <me@112buddyd.dev>
2023-06-23 23:07:15 -0400

    added luna/stars

---
+++ keyboards/boardsource/lulu/keymaps/112buddyd/main-screen.c
@@ -0,0 +1,39 @@
+#pragma once
+
+#ifdef OLED_ENABLE
+    #include "luna.c"
+
+    static void print_status_narrow(void) {
+        oled_set_cursor(0, 1);
+
+        /* Print current layer */
+        oled_write("Layer", false);
+
+        oled_set_cursor(0, 2);
+
+        switch (get_highest_layer(layer_state)) {
+            case 0:
+                oled_write("Base ", false);
+                break;
+            case 1:
+                oled_write("Colem", false);
+                break;
+            case 2:
+                oled_write("Ctrl ", false);
+                break;
+            case 3:
+                oled_write("Numpd", false);
+                break;
+            default:
+                oled_write("Undef", false);
+        }
+
+        oled_set_cursor(0,4);
+        oled_write("WPM", false);
+        oled_set_cursor(0,5);
+        oled_write(get_u8_str(get_current_wpm(), '0'), false);
+
+        render_luna(0,13);
+    }
+
+#endif
--- keyboards/boardsource/lulu/keymaps/112buddyd/rules.mk
+++ keyboards/boardsource/lulu/keymaps/112buddyd/rules.mk
@@ -1,1 +1,6 @@
+WPM_ENABLE = yes					# Enable word per minute counter
+LTO_ENABLE = yes					# Makes hex file smaller
 OLED_ENABLE = yes
+MOUSEKEY_ENABLE = no
+CONSOLE_ENABLE = no
+OCEAN_DREAM_ENABLE = yes
